de5068695a381d117ae3829d9c2698d954b85efc
Haohong Zhang <iceqhdrop@outlook.com>
2026-01-09 19:51:30 +0800

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/HUST-NingKang-Lab/BGC-Prophet'
 EMAIL = 'laiqilong@hust.edu.cn'
 AUTHOR = 'HUST NingKang Lab'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
